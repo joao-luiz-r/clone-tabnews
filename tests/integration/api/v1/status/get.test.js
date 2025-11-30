d44fb0cecd4daece9a3c9c31f59fc38afdf1f9b3
João Luiz Rodrigues <joaoluizr@gmail.com>
2025-11-30 02:09:58 +0000

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,22 @@
 test("GET /api/v1/status should return 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
+
+  const responseBody = await res.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
+
+  const databaseVersionValue = responseBody.dependencies.database.version;
+  expect(databaseVersionValue).toEqual("16.0");
+
+  const databaseMaxConnectionsValue =
+    responseBody.dependencies.database.max_connections;
+  expect(databaseMaxConnectionsValue).toEqual(100);
+
+  const databaseOpenedConnectionsValue =
+    responseBody.dependencies.database.opened_connections;
+  expect(databaseOpenedConnectionsValue).toEqual(1);
+
+  /*console.log("Response Body:", responseBody);*/
 });
