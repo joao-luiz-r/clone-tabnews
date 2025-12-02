0f652e3f39392d6b026793c1d31b8939e87eee0d
João Luiz Rodrigues <joaoluizr@gmail.com>
2025-12-02 22:24:28 +0000

    ajustando arquivos

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
-  const updateAt = new Date().toISOString();
+  const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
@@ -14,14 +14,14 @@ async function status(request, response) {
 
   const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionsResult = await database.query({
-    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
   });
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
-    updated_at: updateAt,
+    updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
